d46490bdbe000ef88afcc8d95b5d50a4396b4cf9
murray-watt <murray-watt@hotmail.com>
2024-03-09 14:22:23 -0700

    deal with utility class false warning

--- src/main/java/org/mwatt/patterns/LazySingleton.java
+++ src/main/java/org/mwatt/patterns/LazySingleton.java
@@ -1,9 +1,16 @@
 package org.mwatt.patterns;
 
+import org.jetbrains.annotations.Contract;
+
+// This is a lazy singleton class, not a utility class
 public class LazySingleton {
     private static volatile LazySingleton instance;
     private static final Object syncLock = new Object();
 
+    @Contract(pure = true)
+    private LazySingleton() {
+        // Exists only to defeat instantiation.
+    }
 
     public static LazySingleton getInstance() {
         if (instance == null) {
